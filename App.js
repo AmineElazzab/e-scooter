fdf7ea4be93d3f70a0fa9e1d8dcda4cf6f4607ea
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-02-16 16:56:25 +0100

    App

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect, useState } from "react";
+import { firebase } from "./config";
+import Login from "./src/Login";
+import Register from "./src/Register";
+import Dashboard from "./src/Dashboard";
+import Header from "./components/Header";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login}
+            options={{
+              headerTitle: () => <Header name="Login"/>,
+              headerStyle: {
+                backgroundColor: '#FFB800',
+                height: 100,
+                // borderBottomLeftRadius: 50,
+                borderBottomRightRadius: 50,
+                shadowColor:'#000',
+                elevation: 25,
+              },
+            }}
+
+          />
+          <Stack.Screen name="Register" component={Register} 
+            options={{
+              headerTitle: () => <Header name="Register" />,
+              headerStyle: {
+                backgroundColor: '#FFB800',
+                height: 100,
+                // borderBottomLeftRadius: 50,
+                borderBottomRightRadius: 50,
+                shadowColor:'#000',
+                elevation: 25,
+              },
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Dashboard" component={Dashboard} 
+          options={{
+            headerTitle: () => <Header name="Dashboard" />,
+            headerStyle: {
+              backgroundColor: '#FFB800',
+              height: 100,
+              // borderBottomLeftRadius: 50,
+              borderBottomRightRadius: 50,
+              shadowColor:'#000',
+              elevation: 25,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default () => {
+  return <App />;
+}
